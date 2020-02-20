2c2e7199f4f32699a937ca12b3df52eb0cdea31a
Michael Bowman <micbowman25@gmail.com>
2020-02-19 21:04:57 -0600

    set up all the employees but haven't tested it yet

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ fs.readFile("List.json", "utf8", function(err, data) {
     const Id = [];
     const Email = [];
     const Role = [];
+
+    ListJSON.forEach(function(Employee) {
+        if(Employee.Name === "Alice") {
+            Name.push(Employee);
+        }
+            else if (Employee.Id === "Alice") {
+                Email.push(Employee)
+            }
+    });
+
+    const ListJSON = JSON.stringify(Employee, null, 2);
+
+    fs.writeFile("List.json", ListJSON, function(err) {
+        if (err) {
+          throw err;
+        }
+    });
 });
